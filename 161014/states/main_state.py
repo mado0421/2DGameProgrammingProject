9943ce4813aa94c0536f9531eea07c09646015d0
mado0421 <sju421@gmail.com>
2016-11-15 21:20:22 +0900

    랭크 scene에서 뒤로 가기 만듬

--- 161014/states/main_state.py
+++ 161014/states/main_state.py
@@ -1,25 +1,28 @@
 from pico2d import *
 import game_framework
 from states import game_state
+from states import ranking_state
 from classes import CButton
 
 name = "MainState"
 back_image = None
-button_list = []
-
+button_list = None
+font = None
 
 def enter():
-    global back_image, button_list
+    global back_image, button_list, font
+    button_list = []
     button_list.append(CButton.Button('start', get_canvas_width()/2, get_canvas_height()/2 - 100, 'start'))
     button_list.append(CButton.Button('option', get_canvas_width()/2, get_canvas_height()/2 - 150, 'game'))
-    button_list.append(CButton.Button('rank', get_canvas_width()/2, get_canvas_height()/2 - 200, 'game'))
-    button_list.append(CButton.Button('quit', get_canvas_width()/2, get_canvas_height()/2 - 250, 'game'))
+    button_list.append(CButton.Button('rank', get_canvas_width()/2, get_canvas_height()/2 - 200, 'rank'))
+    button_list.append(CButton.Button('quit', get_canvas_width()/2, get_canvas_height()/2 - 250, 'quit'))
     back_image = load_image('resource/image/back_gamestate.png')
-
+    font = load_font('resource/font/RPGSystem.ttf', 200)
 
 def exit():
-    global button_list
+    global button_list, font
     del(button_list)
+    del(font)
     pass
 
 def update(frame_time):
@@ -28,7 +31,9 @@ def update(frame_time):
 
 def draw():
     clear_canvas()
+
     back_image.draw(get_canvas_width()/2, get_canvas_height()/2)
+    font.draw(get_canvas_width()/2 - 290, get_canvas_height()/2 + 130, 'ASTEROID', (230, 230, 255))
     for button in button_list:
         button.draw()
     update_canvas()
@@ -44,6 +49,10 @@ def handle_events(frame_time):
                 if button.is_on(event.x, get_canvas_height() - event.y):
                     if button.tag is 'start':
                         game_framework.push_state(game_state)
+                    if button.tag is 'rank':
+                        game_framework.push_state(ranking_state)
+                    if button.tag is 'quit':
+                        game_framework.quit()
         else:
             for button in button_list:
                 button.handle_event(event)
--- 161014/states/ranking_state.py
+++ 161014/states/ranking_state.py
@@ -1,23 +1,26 @@
 import game_framework
 from pico2d import *
+from classes import CButton
 from states import main_state
 
 name = "RankingState"
 image = None
 font = None
-
+button_list = None
 
 def enter():
-    global image
+    global image, button_list, font
+    button_list = []
+    button_list.append(CButton.Button('back', get_canvas_width()/2, get_canvas_height()/2 - 250, 'main'))
     image = load_image('resource/image/back_gamestate.png')
-    global font
     font = load_font('resource/font/RPGSystem.ttf', 50)
 
 
 def exit():
-    global image, font
+    global image, font, button_list
     del(image)
     del(font)
+    del(button_list)
 
 
 def pause():
@@ -33,9 +36,14 @@ def handle_events(frame_time):
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            for button in button_list:
+                if button.is_on(event.x, get_canvas_height() - event.y):
+                    if button.tag is 'main':
+                        game_framework.push_state(main_state)
         else:
-            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
-                game_framework.change_state(main_state)
+            for button in button_list:
+                button.handle_event(event)
 
 
 def update(frame_time):
@@ -51,7 +59,9 @@ def bubble_sort(data):
 
 def draw_ranking():
 
-    f = open('data_file.txt', 'r')
+    font.draw(get_canvas_width()/2 - 90, get_canvas_height()/2 + 290, '[RANKING]', (230, 230, 255))
+
+    f = open('data_score_file.txt', 'r')
     score_data = json.load(f)
     f.close()
 
@@ -59,18 +69,36 @@ def draw_ranking():
 
     score_data = score_data[:10]
 
-    font.draw(get_canvas_width()/2 - 90, get_canvas_height()/2 + 200, '[RANKING]', (255, 255, 255))
+    font.draw(get_canvas_width()/2 - 90 + 200, get_canvas_height()/2 + 250, '[SCORE]', (230, 230, 255))
     y = 0
     for score in score_data:
-        font.draw(get_canvas_width()/2 - 100, 100 + get_canvas_height()/2 - 40 * y,
+        font.draw(get_canvas_width()/2 - 100 + 200, 200 + get_canvas_height()/2 - 40 * y,
                   'SCORE:%5.1d' % score['Score'],
-                  (255, 255, 255))
+                  (190, 190, 205))
+        y += 1
+
+    f = open('data_time_file.txt', 'r')
+    score_data = json.load(f)
+    f.close()
+
+    bubble_sort(score_data)
+
+    score_data = score_data[:10]
+
+    font.draw(get_canvas_width()/2 - 90 - 200, get_canvas_height()/2 + 250, '[TIME]', (230, 230, 255))
+    y = 0
+    for score in score_data:
+        font.draw(get_canvas_width()/2 - 100 - 200, 200 + get_canvas_height()/2 - 40 * y,
+                  'Time:%5.1f' % score['Score'],
+                  (190, 190, 205))
         y += 1
 
 
 def draw():
-    global image, font
+    global image, font, button_list
     clear_canvas()
     image.draw(get_canvas_width()/2, get_canvas_height()/2)
     draw_ranking()
+    for button in button_list:
+        button.draw()
     update_canvas()
