f1e3234b452710a25790a69e9f36b67410129cfe
mado0421 <sju421@gmail.com>
2016-12-22 22:17:35 +0900

    캐릭터 선택 버튼 입력 받기

--- Asteroid/Object/CPortrait.py
+++ Asteroid/Object/CPortrait.py
@@ -2,12 +2,21 @@ from pico2d import load_image
 
 
 class Portrait:
+
+    char0, char1 = 0, 1
+
     def __init__(self, x, y, num_player):
         self.x, self.y = x, y
         self.char_frame = load_image('Resource/image/char_frame.png')
         self.ship_frame = load_image('Resource/image/ship_frame.png')
         self.char_img = load_image('Resource/image/char_%d.png' % num_player)
         self.ship_img = load_image('Resource/image/ship%d.png' % num_player)
+        if num_player is self.char0:
+            self.speed = 4
+            self.rotate = 2
+        elif num_player is self.char1:
+            self.speed = 3
+            self.rotate = 3
 
     def draw(self):
         self.char_frame.draw(self.x, self.y)
--- Asteroid/Scene/character_scene.py
+++ Asteroid/Scene/character_scene.py
@@ -6,17 +6,34 @@ from Scene import main_scene
 
 font = None
 back_img = None
+gauge_img = None
 portrait_list = []
+player_list = []
+selectP1 = 0
+selectP2 = 0
 
 def enter():
-    global font, back_img, portrait_list
+    global font, back_img, portrait_list, gauge_img
+    global selectP1, selectP2
     portrait_list = []
 
-    portrait_list.append(CPortrait.Portrait(get_canvas_width()/2 - 300, get_canvas_height()/2 + 100, 0))
-    portrait_list.append(CPortrait.Portrait(get_canvas_width()/2 + 300, get_canvas_height()/2 + 100, 1))
+    f = open('game_data.txt', 'r')
+    num_player = json.load(f)
+    f.close()
+
+    if num_player is 1:
+        selectP1 = 0
+        selectP2 = -1
+    elif num_player is 2:
+        selectP1 = 0
+        selectP2 = 0
+
+    portrait_list.append(CPortrait.Portrait(get_canvas_width()/2 - 200, get_canvas_height()/2 + 150, 0))
+    portrait_list.append(CPortrait.Portrait(get_canvas_width()/2 + 200, get_canvas_height()/2 + 150, 1))
 
     font = load_font('Resource/font/RPGSystem.ttf', 50)
     back_img = load_image('resource/image/backimg.png')
+    gauge_img = load_image('resource/image/gauge.png')
     pass
 
 
@@ -34,11 +51,31 @@ def draw():
     if len(portrait_list) is not 0:
         for portrait in portrait_list:
             portrait.draw()
-
+            font.draw(
+                get_canvas_width() / 2 - 300 + 400 * portrait_list.index(portrait),
+                get_canvas_height() / 2,
+                'SPEED', (230, 230, 255))
+            font.draw(
+                get_canvas_width() / 2 - 300 + 400 * portrait_list.index(portrait),
+                get_canvas_height() / 2 - 50,
+                'ROTATE', (230, 230, 255))
+            font.draw(
+                get_canvas_width() / 2 - 250 + 400 * portrait_list.index(portrait),
+                get_canvas_height() / 2 - 120,
+                'SELECT', (230, 230, 255))
+            for i in range(0, portrait.speed):
+                gauge_img.draw(
+                    get_canvas_width() / 2 - 140 + 400 * portrait_list.index(portrait) + 25 * i,
+                    get_canvas_height() / 2)
+            for i in range(0, portrait.rotate):
+                gauge_img.draw(
+                    get_canvas_width() / 2 - 140 + 400 * portrait_list.index(portrait) + 25 * i,
+                    get_canvas_height() / 2 - 50)
     update_canvas()
 
 
 def handle_events(frame_time):
+    global selectP1, selectP2
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -46,6 +83,16 @@ def handle_events(frame_time):
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 game_framework.quit()
+            elif event.key == SDLK_a:
+                selectP1 = clamp(1, selectP1 + 1, 2)
+            elif event.key == SDLK_d:
+                selectP1 = clamp(1, selectP1 - 1, 2)
+            elif event.key == SDLK_LEFT:
+                if selectP2 is not -1:
+                    selectP2 = clamp(1, selectP2 + 1, 2)
+            elif event.key == SDLK_RIGHT:
+                if selectP2 is not -1:
+                    selectP2 = clamp(1, selectP2 - 1, 2)
         pass
 
 
